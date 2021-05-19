ebfe775c2318dca2e8b981f7e68ff26fdff5c606
Wenqi Niu <oreki@WenqideMacBook-Pro.local>
2021-05-18 22:22:42 -0400

    Added PostgreSQL environment variable.

--- app.py
+++ app.py
@@ -9,31 +9,21 @@ from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
+
 app = Flask(__name__)
-
-app.config['DEBUG'] = True
-
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI').replace("://", "ql://", 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = 'jose'
+app.secret_key = 'alex'
 api = Api(app)
 
 jwt = JWT(app, authenticate, identity)  # /auth
 
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(Item, '/item/<string:name>')
-api.add_resource(ItemList, '/items')
 api.add_resource(StoreList, '/stores')
+api.add_resource(ItemList, '/items')
 
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
-    from db import db
-    db.init_app(app)
-
-    if app.config['DEBUG']:
-        @app.before_first_request
-        def create_tables():
-            db.create_all()
-
-    app.run(port=5000)
+    app.run(port=5000, debug=True)
